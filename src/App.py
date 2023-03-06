01136543f84064071af36791a6f91750ed757a02
novenopatch <homawoojoseph@gmail.com>
2023-03-06 13:56:05 +0000

    start gui

---
+++ src/App.py
@@ -0,0 +1,216 @@
+import tkinter
+
+import customtkinter
+from PIL import Image
+import requests
+from io import BytesIO
+import os
+
+from pytube import Stream, YouTube
+
+from src.main import YTDown
+from src.save import Save
+
+
+class CheckBoxItem(customtkinter.CTkFrame):
+    def __init__(self, master,youtube:YouTube,stream:Stream,  **kwargs):
+        super().__init__(master, **kwargs)
+        self.youtube = youtube
+        self.title = youtube.title
+        self.stream = stream
+        self.checked = tkinter.BooleanVar()
+        self.checked.set(False)
+        response = requests.get(youtube.thumbnail_url)
+        image_data = response.content
+        my_image = customtkinter.CTkImage(light_image=Image.open(BytesIO(image_data)),
+                                          dark_image=Image.open(BytesIO(image_data)),
+                                          size=(90, 90))
+        self.button = customtkinter.CTkLabel(self, image=my_image,text="")
+        self.checkbox = customtkinter.CTkCheckBox(self, text=self.title,variable=self.checked)
+        self.button.pack(side="left", padx=5)
+        self.checkbox.pack(side="left", padx=5)
+    def configure_command(self, command):
+        self.checkbox.configure(command=command)
+        #self.button.configure(command=command)
+
+    def is_checked(self):
+        return self.checked.get()
+    def get_title(self):
+        return self.title
+
+
+class CheckBoxFrame(customtkinter.CTkFrame):
+    def __init__(self, master, item_list:list[(YouTube,Stream)], command=None, **kwargs):
+        super().__init__(master, **kwargs)
+        self.command = command
+        self.checkbox_list = []
+        for youtube, stream_choice in item_list:
+            self.add_item(youtube,stream_choice)
+
+    def add_item(self, youtube:YouTube,stream:Stream):
+        item_checkbox = CheckBoxItem(self, youtube,stream)
+        if self.command is not None:
+            item_checkbox.configure_command(command=self.command)
+
+        item_checkbox.grid(row=len(self.checkbox_list), column=0, pady=(0, 10))
+        #button = customtkinter.CTkButton(item_frame, image=my_image,fg_color="transparent",text="")
+        self.checkbox_list.append( item_checkbox)
+
+
+
+    def get_checked_items(self):
+        return [( checkbox.youtube,checkbox.stream ) for checkbox in self.checkbox_list if checkbox.is_checked()]
+
+
+class App(customtkinter.CTk):
+    def __init__(self,save: Save):
+        self.save = save
+        super().__init__()
+
+        self.title("YDOWN.py")
+        self.geometry("720x480")
+
+
+        self.navigation_frame = customtkinter.CTkFrame(self, corner_radius=0)
+
+        self.home_frame = customtkinter.CTkScrollableFrame(self, corner_radius=0, fg_color="transparent")
+
+        self.downloads_frame = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
+
+        self.home_button = customtkinter.CTkButton(self.navigation_frame, corner_radius=0, height=40, border_spacing=10,
+                                                   text="Home",
+                                                   fg_color="transparent", text_color=("gray10", "gray90"),
+                                                   hover_color=("gray70", "gray30"),
+                                                   anchor="w", command=self.home_button_event)
+        self.downloads_button = customtkinter.CTkButton(self.navigation_frame, corner_radius=0, height=40,
+                                                        border_spacing=10,
+                                                        text="Downloads",
+                                                        fg_color="transparent", text_color=("gray10", "gray90"),
+                                                        hover_color=("gray70", "gray30"),
+                                                        anchor="w", command=self.downloads_button_event)
+
+        self.appearance_mode_menu = customtkinter.CTkOptionMenu(self.navigation_frame,
+                                                                values=["Light", "Dark", "System"],
+                                                                command=self.change_appearance_mode_event)
+
+        self.progressbar = customtkinter.CTkProgressBar(self.home_frame)
+
+        self.url_input_frame = customtkinter.CTkFrame(self.home_frame)
+        self.radiobutton_frame = customtkinter.CTkFrame(self.home_frame)
+        self.entry_url = customtkinter.CTkEntry(self.url_input_frame, placeholder_text="Past url")
+        self.entry_output = customtkinter.CTkEntry(self.url_input_frame, placeholder_text="Output")
+        self.label_output = customtkinter.CTkLabel(self.url_input_frame, text="Folder")
+        self.radio_var = tkinter.IntVar(value=0)
+        self.label_radio_group = customtkinter.CTkLabel(master=self.radiobutton_frame, text="Choice type")
+        self.radio_button_1 = customtkinter.CTkRadioButton(master=self.radiobutton_frame, variable=self.radio_var,
+                                                           value=0, text="Video")
+        self.radio_button_2 = customtkinter.CTkRadioButton(master=self.radiobutton_frame, variable=self.radio_var,
+                                                           value=1, text="Playlist")
+
+        self.download_button = customtkinter.CTkButton(self.home_frame, corner_radius=0, height=40,
+                                                       border_spacing=10,
+                                                       text="Download",
+                                                       fg_color="#f44336", text_color=("#fffff", "#ffffff"),
+                                                       hover_color=("#ba000d", "#ff7961"),
+                                                       command=self.downloads_button_event)
+
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=1)
+
+        self.navigation_frame.grid(row=0, column=0, sticky="nsew")
+        self.navigation_frame.grid_rowconfigure(3, weight=1)
+        self.home_frame.grid_columnconfigure(0, weight=1, )
+        self.downloads_frame.grid_columnconfigure(0, weight=1)
+        self.home_button.grid(row=1, column=0, sticky="ew")
+        self.downloads_button.grid(row=2, column=0, sticky="ew")
+        self.appearance_mode_menu.grid(row=6, column=0, padx=20, pady=20, sticky="s")
+        self.progressbar.grid(row=1, column=0, padx=20, pady=10)
+        self.url_input_frame.grid(row=2, column=0, padx=(20, 20), pady=(20, 0), sticky="nsew")
+        self.radiobutton_frame.grid(row=3, column=0, padx=(20, 20), pady=(20, 0), sticky="nsew")
+        self.download_button.grid(row=5, padx=(20, 20), pady=(20, 0), )
+
+        self.url_input_frame.grid_rowconfigure(0, weight=1)
+        self.url_input_frame.grid_columnconfigure(3, weight=1)
+        self.label_url = customtkinter.CTkLabel(self.url_input_frame, text="url:")
+
+        self.label_url.grid(row=0, column=0, padx=10, pady=10)
+        self.entry_url.grid(row=0, column=2, columnspan=2, padx=10, pady=10, sticky="nsew")
+        self.label_output.grid(row=1, column=0, padx=10, pady=10)
+        self.entry_output.grid(row=1, column=2, columnspan=2, padx=10, pady=10, sticky="nsew")
+
+        self.label_radio_group.grid(row=0, column=1, columnspan=1, padx=10, pady=10, sticky="")
+        self.radio_button_1.grid(row=0, column=2, pady=10, padx=20, sticky="n")
+        self.radio_button_2.grid(row=0, column=3, pady=10, padx=20, sticky="n")
+
+        self.add_option_frame()
+        self.add_choice_frame()
+        self.home_frame.grid(row=0, column=1, sticky="nsew", padx=20, pady=20)
+
+    def add_option_frame(self):
+
+        self.option_frame = customtkinter.CTkFrame(self.home_frame)
+        self.option_frame.grid(row=4, column=0, padx=(20, 20), pady=(20, 0), sticky="nsew")
+        self.option_frame.grid_rowconfigure(1, weight=1)
+        self.option_frame.grid_columnconfigure(5, weight=1)
+        self.label_resolution = customtkinter.CTkLabel(self.option_frame, text="resolution:")
+        self.label_type = customtkinter.CTkLabel(self.option_frame, text="type:")
+        self.switch_var = customtkinter.StringVar(value="audio")
+        self.switch = customtkinter.CTkSwitch(master=self.option_frame,
+                                              text="audio",
+                                              command=self.switch_event, variable=self.switch_var, onvalue="audio",
+                                              offvalue="video")
+        self.combobox_2 = customtkinter.CTkOptionMenu(master=self.option_frame,
+                                                      values=["144p", "240p", "360p", "480p", "720p", "1080p"],
+                                                      command=self.optionmenu_callback)
+        self.label_type.grid(row=1, column=1, padx=5, pady=5)
+        self.switch.grid(row=1, column=2, padx=5, pady=5)
+        self.label_resolution.grid(row=1, column=3, padx=5, pady=5)
+        self.combobox_2.grid(row=1, column=4, padx=5, pady=5)
+    def add_choice_frame(self):
+        yt_down = YTDown(self.save,True)
+        self.checkbox_frame = CheckBoxFrame(master=self.home_frame,
+                                                                 command=self.checkbox_frame_event,
+                                                                 item_list=[f"item {i}" for i in range(10)])
+        self.checkbox_frame.grid(row=6, column=0, padx=10, pady=10, sticky="nsew")
+        self.checkbox_frame.add_item("new item")
+
+    def optionmenu_callback(self,choice):
+        print("optionmenu dropdown clicked:", choice)
+
+    def select_frame_by_name(self, name):
+        # set button color for selected button
+        self.home_button.configure(fg_color=("gray75", "gray25") if name == "home" else "transparent")
+        self.downloads_button.configure(fg_color=("gray75", "gray25") if name == "frame_2" else "transparent")
+
+        # show selected frame
+        if name == "home":
+            self.home_frame.grid(row=0, column=1, sticky="nsew")
+        else:
+            self.home_frame.grid_forget()
+        if name == "frame_2":
+            self.downloads_frame.grid(row=0, column=1, sticky="nsew")
+        else:
+            self.downloads_frame.grid_forget()
+
+    def change_appearance_mode_event(self, new_appearance_mode):
+        customtkinter.set_appearance_mode(new_appearance_mode)
+
+    def home_button_event(self):
+        self.select_frame_by_name("home")
+        print("home")
+
+    def downloads_button_event(self):
+        self.select_frame_by_name("downloads")
+        print("download")
+
+    def checkbox_frame_event(self):
+        print(f"checkbox frame modified: {self.checkbox_frame.get_checked_items()}")
+
+    def switch_event(self):
+        self.switch.configure(text=self.switch_var.get())
+        print("switch toggled, current value:", self.switch_var.get())
+
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
--- src/main.py
+++ src/main.py
@@ -11,28 +11,35 @@ import argparse
 
 from Enumeration import SaveData
 from save import Save
+from colorama import init
+from termcolor import colored
 
-WELCOME_STR: str = """ __   __  _______    ______   _______  _     _  __    _ 
-    |  | |  ||       |  |      | |       || | _ | ||  |  | |
-    |  |_|  ||_     _|  |  _    ||   _   || || || ||   |_| |
-    |       |  |   |    | | |   ||  | |  ||       ||       |
-    |_     _|  |   |    | |_|   ||  |_|  ||       ||  _    |
-      |   |    |   |    |       ||       ||   _   || | |   |
-      |___|    |___|    |______| |_______||__| |__||__|_|  |__|
+WELCOME_STR: str = """ 
+            
+  wWw  wWw   _     .-.    (O))  ((O)\\\  /// 
+  (O)  (O) _||\  c(O_O)c   ||    || ((O)(O)) 
+  ( \  / )(_'\  ,'.---.`,  || /\ ||  | \ ||  
+   \ \/ / .'  |/ /|_|_|\ \ ||//\\||  ||\\||  
+    \o / ((_) || \_____/ | / /  \ \  || \ |  
+   _/ /   `-`.)'. `---' .`( /    \ ) ||  ||  
+  (_.'       (   `-...-'   )      ( (_/  \_) 
+
         """
 
 
 class YTDown:
-    def __init__(self, save: Save):
+    def __init__(self, save: Save,graph_mode=False):
         self.save = save
         self.output_path: str = ""
         self.download_resolutions = ["144p", "240p", "360p", "480p", "720p", "1080p"]
         self.select_resolution = None
+        self.graph_mod = graph_mode
+        self.download_streamQuery: list[tuple[YouTube, StreamQuery]] = []
 
     def display_banner(self):
-        print(WELCOME_STR)
+        self.display_message(WELCOME_STR)
         for i, choice in enumerate(self.save.OPTIONS_CHOICE_STR):
-            print(f"{i + 1}): {choice}")
+            self.display_message(f"{i + 1}): {choice}")
 
     def validate_url(self, link: str) -> str:
         pattern = re.compile(
@@ -62,13 +69,13 @@ class YTDown:
     def download_audio_file(self, yt: YouTube, video_file: str, file_extension: str):
         try:
             audio = yt.streams.get_audio_only()
+            self.display_message(f"{self.save.get_message('START_DOWNLOAD_MSG')}: {yt.title}-(Audio)")
             yt.register_on_progress_callback(self.on_progress)
-            print(f"{self.save.get_message('START_DOWNLOAD_MSG')}: {yt.title}-(Audio)")
             audio_file = audio.download(
                 output_path=self.output_path, filename=f"{self.norm_file_name(yt.title)}.mp3"
             )
             if self.save.get_data(SaveData.DEBUG):
-                print(f" yt{yt} , video-path: {video_file}, audio_path: {audio_file}")
+                self.display_message(f" yt{yt} , video-path: {video_file}, audio_path: {audio_file}")
             if audio_file:
                 name_file = f"{video_file}.{file_extension}"
                 output_file = os.path.join(self.output_path,
@@ -77,10 +84,11 @@ class YTDown:
                 subprocess.call(cmd, shell=True)
                 self.after_download_audio(video_file, audio_file, output_file)
         except Exception as e:
-            print(f"{self.save.get_message('SORRY_ERROR_MSG')}:{str(e)}")
+            self.display_message(f"{self.save.get_message('SORRY_ERROR_MSG')}:{str(e)}")
 
     def download(self, youtube: YouTube, video_choice: Stream):
-        print(f"{self.save.get_message('START_DOWNLOAD_MSG')}:-{youtube.title}")
+        self.display_message(f"{self.save.get_message('START_DOWNLOAD_MSG')}:-{youtube.title}")
+        youtube.register_on_progress_callback(self.on_progress)
         video_file = video_choice.download(
             output_path=self.output_path,
             filename=self.norm_file_name(
@@ -89,19 +97,22 @@ class YTDown:
 
         if not video_choice.is_progressive:
             self.download_audio_file(youtube, video_file, f"{video_choice.mime_type.split('/')[-1]}")
-        print(f"Video saved at {self.output_path}")
+        self.display_message(f"Video saved at {self.output_path}")
 
     def print_stream(self, streams: StreamQuery):
         if streams:
-            print(self.save.get_message('RESOLUTIONS_MSG') + ":")
+            self.display_message(self.save.get_message('RESOLUTIONS_MSG') + ":")
             for i, stream in enumerate(streams):
-                print(f"{i + 1}) .{stream.mime_type.split('/')[-1]} {stream.resolution}-{stream.fps}fps")
+                self.display_message(f"{i + 1}) .{stream.mime_type.split('/')[-1]} {stream.resolution}-{stream.fps}fps")
         else:
-            print(f"{self.save.get_message('SORRY_ERROR_MSG')} ")
+            self.display_message(f"{self.save.get_message('SORRY_ERROR_MSG')} ")
 
-    def filter_streams(self, youtube: YouTube) -> StreamQuery:
-        return youtube.streams.filter(file_extension=self.save.get_data(SaveData.FILE_EXTENSION)).order_by(
-            'resolution', )
+    def filter_streams(self, youtube: YouTube, resolution: str = None) -> StreamQuery:
+        return youtube.streams.filter(
+            subtype=self.save.get_data(
+                SaveData.FILE_EXTENSION), resolution=self.select_resolution if not self.graph_mod else resolution
+        ).order_by(
+            'resolution')
 
     def print_available_resolution_and_select(self, youtube: YouTube, streams: StreamQuery) -> Stream:
         self.print_stream(streams)
@@ -125,18 +136,18 @@ class YTDown:
 
                 if self.save.get_data(SaveData.DEBUG):
                     for stream in streams:
-                        print(f"{stream.resolution} --reso_select: {self.select_resolution}--choice: {video_choice}")
+                        self.display_message(
+                            f"{stream.resolution} --reso_select: {self.select_resolution}--choice: {video_choice}")
                 if not video_choice:
                     video_choice = self.print_available_resolution_and_select(youtube, streams)
 
             if not is_playlist:
-                youtube.register_on_progress_callback(self.on_progress)
                 self.download(youtube, video_choice)
             else:
                 return youtube, video_choice
 
         except Exception as e:
-            print(f" {self.save.get_message('SORRY_ERROR_MSG')} : {youtube.title}.{str(e)}")
+            self.display_message(f" {self.save.get_message('SORRY_ERROR_MSG')} : {youtube.title}.{str(e)}")
 
     def download_video_file(self, youtube: YouTube):
         streams = self.filter_streams(youtube)
@@ -147,40 +158,62 @@ class YTDown:
         length = len(youtube_list)
         for i, youtube in enumerate(youtube_list):
             streams = self.filter_streams(youtube)
-            print(f"{i + 1}/{length})-{youtube.title}")
+            self.display_message(f"{i + 1}/{length})-{youtube.title}")
 
             downloads.append(self.choice_and_download(youtube, streams, True))
 
         for youtube, video_choice in downloads:
-            youtube.register_on_progress_callback(self.on_progress)
             try:
                 self.download(youtube, video_choice)
             except Exception as e:
-                print(f"{self.save.get_message('SORRY_ERROR_MSG')}: {str(e)}'")
+                self.display_message(f"{self.save.get_message('SORRY_ERROR_MSG')}: {str(e)}'")
 
-        print(self.save.get_message('END_MESSAGE'))
-
-    def download_single_video(self, link: str):
-        youtube = YouTube(link)
-        self.download_video_file(youtube)
+        self.display_message(self.save.get_message('END_MESSAGE'))
 
     def print_title(self, title: str):
-        print(f"Title:{title}")
+        self.display_message(f"Title:{title}")
 
-    def download_playlist(self, link: str):
+    def save_streams(self, youtube: YouTube, resolution: str):
+        self.download_streamQuery.append(
+            (youtube, self.filter_streams(youtube, resolution))
+        )
+
+    def save_list_of_streams(self, youtubes: Iterable[YouTube], resolution: str):
+        for i, youtube in enumerate(youtubes):
+            self.download_streamQuery.append(
+                (youtube, self.filter_streams(youtube, resolution))
+            )
+
+    def download_single_video(self, link: str) -> YouTube | None:
+        youtube = YouTube(link)
+        if not self.graph_mod:
+            self.download_video_file(youtube)
+        else:
+            return youtube
+
+    def download_playlist(self, link: str) -> Iterable[YouTube] | None:
         playlist = Playlist(link)
         self.print_title(playlist.title)
         self.output_path = os.path.join(self.output_path, self.norm_file_name(playlist.title))
-        self.download_videos_file(playlist.videos)
+        if not self.graph_mod:
+            self.download_videos_file(playlist.videos)
+        else:
+            return playlist.videos
 
     def on_progress(self, stream, chunk, bytes_remaining):
         total_size = stream.filesize
         bytes_downloaded = total_size - bytes_remaining
 
         progress = bytes_downloaded / total_size * 100
-        print(
-            f"\r {self.save.get_message('DOWNLOADING_MSG')} : {progress:.2f}%( {(bytes_downloaded / 1024 / 1024):.2f}Mb/{(total_size / 1024 / 1024):.2f}Mb )",
+        self.display_message(
+            f"\r {self.save.get_message('DOWNLOADING_MSG')} {progress:.2f}%: {(bytes_downloaded / 1024 / 1024):.2f}Mb/{(total_size / 1024 / 1024):.2f}Mb ",
             end="")
+        if self.graph_mod:
+            return {
+                "progress": progress,
+                "bytes_downloaded": (bytes_downloaded / 1024 / 1024),
+                "total_size": (total_size / 1024 / 1024)
+            }
 
     def launch(self, link: str, choice: int, output_path: str, selected_resolution: str | None = None):
 
@@ -197,21 +230,25 @@ class YTDown:
             else:
                 self.download_playlist(link)
         except Exception as e:
-            print(f"il semble avoir un souci avec votre lien . Erreur: {str(e)}")
+            self.display_message(f"il semble avoir un souci avec votre lien . Erreur: {str(e)}")
 
     def display_available_resolution(self) -> str:
         for i, resol in enumerate(self.download_resolutions):
-            print(f"{i + 1}) {resol}")
+            self.display_message(f"{i + 1}) {resol}")
         choice = input(f"{self.save.get_message('YOUR_RESOLUTION_CHOICE_MSG')}:")
         try:
             return self.download_resolutions[int(choice) - 1]
         except Exception as e:
-            print(f"{str(e)}")
+            self.display_message(f"{str(e)}")
             return self.download_resolutions[len(self.download_resolutions) - 1]
 
     def display_choice_msg(self):
         self.display_banner()
         return input(self.save.get_message('YOUR_CHOICE_MSG'))
+
+    def display_message(self, message: str):
+        if not yt_down.graph_mod:
+            print(message)
 
 
 if __name__ == '__main__':
@@ -221,15 +258,15 @@ if __name__ == '__main__':
     parser.add_argument('-l', '--link', type=str, nargs='?', help='le lien de la video ou la playliste')
     parser.add_argument('-o', '--output_dir', type=str, nargs='?', help='le dossier de la video')
     parser.add_argument('-r', '--video_resolution', type=str, nargs='?', help='La résolution de la vidéo ex: 1080p')
+    if not yt_down.graph_mod:
+        video_resolution = None
+        args = parser.parse_args()
+        choice = args.url_type if args.url_type else yt_down.display_choice_msg()
+        link = args.link if args.link else input("Lien de la vidéo ou le Lien de la playliste : ")
+        output_path = args.output_dir if args.output_dir else input(
+            "Chemin de téléchargement (laisser vide pour utiliser le répertoire courant) : ").strip()
+        video_resolution = args.video_resolution
 
-    video_resolution = None
-    args = parser.parse_args()
-    choice = args.url_type if args.url_type else yt_down.display_choice_msg()
-    link = args.link if args.link else input("Lien de la vidéo ou le Lien de la playliste : ")
-    output_path = args.output_dir if args.output_dir else input(
-        "Chemin de téléchargement (laisser vide pour utiliser le répertoire courant) : ").strip()
-    video_resolution = args.video_resolution
-
-    if not output_path:
-        output_path = os.getcwd()
-    yt_down.launch(link, int(choice), output_path, video_resolution)
+        if not output_path:
+            output_path = os.getcwd()
+        yt_down.launch(link, int(choice), output_path, video_resolution)
